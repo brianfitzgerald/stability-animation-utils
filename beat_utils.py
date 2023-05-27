00bd3b37dce5afa498586045f05ee60841901d94
DESKTOP-FP8LE75\coold <cooldude242@gmail.com>
2023-05-27 16:10:52 -0500

    move fns to separate file

--- beat_utils.py
+++ beat_utils.py
@@ -1,54 +1,68 @@
 from typing import List
 import numpy as np
 
+
 def sample_array(arr, rate):
-  arr = np.array(arr)
-  if rate < 1:
-    rate = int(1 / rate)
-    num_samples = rate - 1
-    print(num_samples)
-    sampled = np.hstack([np.linspace(arr[i], arr[i+1], num_samples+2, endpoint=False)[1:] for i in range(len(arr)-1)])
-    sampled = np.hstack([arr, sampled])
-    sampled.sort()
-  else:
-    sampled = arr[::rate]
-  return sampled
+    arr = np.array(arr)
+    if rate < 1:
+        rate = int(1 / rate)
+        num_samples = rate - 1
+        print(num_samples)
+        sampled = np.hstack(
+            [
+                np.linspace(arr[i], arr[i + 1], num_samples + 2, endpoint=False)[1:]
+                for i in range(len(arr) - 1)
+            ]
+        )
+        sampled = np.hstack([arr, sampled])
+        sampled.sort()
+    else:
+        sampled = arr[::rate]
+    return sampled
+
 
 fps = 24
+
 
 # times are in sec
 # adsr_times = decay, release
 # adsr_mags = on_value, decay_value, off_value
-def beats_deforum(beats, adsr_times: List[float], adsr_mags: List[float], every_n: int = 1):
-  decay, release = adsr_times
-  on_value, decay_value, off_value = adsr_mags
-  frames = [(0, off_value)]
-  beats = sample_array(beats, every_n)
-  for i in range(0, len(beats)):
-    time = round(beats[i], 3)
-    keyframes = np.transpose((np.round([time, time + decay, time + decay + release], 3), [on_value, decay_value, off_value]))
-    frames.extend([(int(k[0] * fps), k[1]) for k in keyframes])
-  
-  frames = sorted(frames, key=lambda x: x[0])
+def beats_deforum(
+    beats, adsr_times: List[float], adsr_mags: List[float], every_n: int = 1, fps=24
+):
+    decay, release = adsr_times
+    on_value, decay_value, off_value = adsr_mags
+    frames = [(0, off_value)]
+    beats = sample_array(beats, every_n)
+    for i in range(0, len(beats)):
+        time = round(beats[i], 3)
+        keyframes = np.transpose(
+            (
+                np.round([time, time + decay, time + decay + release], 3),
+                [on_value, decay_value, off_value],
+            )
+        )
+        frames.extend([(int(k[0] * fps), k[1]) for k in keyframes])
 
-  # remove duplicate time entries
-  found_keyframes = set()
-  for kf, val in frames:
-    if kf in found_keyframes:
-      frames.remove((kf, val))
-    else:
-      found_keyframes.add(kf)
-  frames = [f"{frame}:({mag})" for frame, mag in frames]
-  return ",".join(frames)
+    frames = sorted(frames, key=lambda x: x[0])
 
-def beats_prompts(beat_times, prompts):
-  prompt_idx = 0
-  res = {
-      0: prompts[0]
-  }
-  for i in range(len(beat_times)):
-    time = round(beat_times[i], 3)
-    frame = int(time * fps)
-    prompt_idx = 0 if prompt_idx == len(prompts) - 1 else prompt_idx + 1
-    res[frame] = prompts[prompt_idx]
-  return res
+    # remove duplicate time entries
+    found_keyframes = set()
+    for kf, val in frames:
+        if kf in found_keyframes:
+            frames.remove((kf, val))
+        else:
+            found_keyframes.add(kf)
+    frames = [f"{frame}:({mag})" for frame, mag in frames]
+    return ",".join(frames)
+
+
+def beats_prompts(beat_times, prompts, fps=24):
+    prompt_idx = 0
+    res = {0: prompts[0]}
+    for i in range(len(beat_times)):
+        time = round(beat_times[i], 3)
+        frame = int(time * fps)
+        prompt_idx = 0 if prompt_idx == len(prompts) - 1 else prompt_idx + 1
+        res[frame] = prompts[prompt_idx]
+    return res
